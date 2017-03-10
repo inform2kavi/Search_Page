c0fb19d2790da0e96c499db89f185d92460d6026
inform2kavi <inform2kavi@gmail.com>
2017-03-10 13:34:04 +1100

    Create ConfigurationFile.java

---
+++ ConfigurationFile.java
@@ -0,0 +1,18 @@
+package utilityPack;
+
+//Configuration class file. It is a static class which contains configuration info such as browser name, home page ur.
+public class ConfigurationFile {
+	
+	public static class URL
+	{
+	//Application URL
+		public static String app_url = "http://www.homeimprovementpages.com.au/search/arborists/nsw/wollongong";
+	}
+	
+	public static class Browser
+	{
+		//which browser
+		public static String browser = "chrome";
+	}
+
+}
